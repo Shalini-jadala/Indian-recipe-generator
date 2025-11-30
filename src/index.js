b3b7bcef5b51b91a1399878dd86b772e7bd37114
Shalini-jadala <jspforever143@gmail.com>
2025-11-30 23:52:34 +1100

    added use interface

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ new Typewriter("#recipe", {
   strings: response.data.answer,
   autoStart: true,
   delay: 2,
+  cursor:"",
 });
 
 }
@@ -19,8 +20,14 @@ function generateRecipe(event){
 let inputs =document.querySelector("#user-input");
 let apiKey = "0a8c6e7ct4a98df4436abe8fo0c14f17";
 let prompt = `user instructions: Generate a recipe about ${inputs.value}`;
-let context = `you are a very good cooking expert and love to write testy recipes. Your mission is to generate a short recipes in basic HTML.Make sure to use user instructions. Do not include html on top of page.Sign the recipe with 'SheCodes AI' inside a <strong> element`;
+let context = `you are a very good cooking expert and love to write testy recipes. Your mission is to generate a short recipes with emojis in basic HTML.Make sure to use user instructions. Do not include html on top of page.Sign the recipe with 'SheCodes AI' inside a <strong> element and place it in center`;
 let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+
+let recipeElement = document.querySelector("#recipe");
+recipeElement.classList.remove("hidden");
+recipeElement.innerHTML = `Generating Recipe <span class = "generating">&#x1F9C9 &#x1F9C9 . . .</span>`;
+
 
 console.log("Generating recipe");
 console.log(`prompt: ${prompt}`);
